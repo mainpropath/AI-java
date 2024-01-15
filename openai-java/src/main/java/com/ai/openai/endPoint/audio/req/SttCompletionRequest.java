825c96c6ec2cbb1b566c743539de2a96cd17b592
jlh <1486097005@qq.com>
2024-01-15 15:39:22 +0800

    1、定义星火大模型聊天请求参数

--- openai-java/src/main/java/com/ai/openai/endPoint/audio/req/SttCompletionRequest.java
+++ openai-java/src/main/java/com/ai/openai/endPoint/audio/req/SttCompletionRequest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import lombok.experimental.FieldNameConstants;
 
 import java.io.File;
 import java.io.Serializable;
@@ -12,6 +13,7 @@ import java.io.Serializable;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldNameConstants
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SttCompletionRequest implements Serializable {
